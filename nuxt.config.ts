bbe09fd9a7fcf2ac52fc3c491dffcf75535bc69c
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-19 14:52:08 +0200

    :heavy_plus_sign: Added nuxt config with i18n config for en lang.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: [
+    '@nuxtjs/i18n',
+    '@pinia/nuxt'
+  ],
+  compatibilityDate: '2024-09-19',
+  i18n: {
+    locales: [
+      { code: 'en', name: 'English', language: 'en-GB', file: 'en.json' },
+      // { code: 'fr', name: 'Français', iso: 'fr-FR', file: 'fr.json' },
+    ],
+    defaultLocale: 'en',
+    lazy: true,
+    langDir: 'assets/langs/',
+    strategy: 'prefix'
+  }
+});
