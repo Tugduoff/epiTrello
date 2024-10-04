c3a31f6272985ad58457baff29e58018689fb650
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-04 18:04:43 +0200

    :heavy_plus_sign: Default config file for tailwind with some custom colors from tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      slate: colors.slate
+    },
+  },
+  plugins: [],
+}
