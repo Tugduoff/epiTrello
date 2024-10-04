6c63a1fa08d47af3c4eb57dc2fc113d099808876
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-04 17:55:16 +0200

    :lipstick: Added default style for html elements like h1, p and h2.

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <BaseLayout>
+    <h1 class="text-2xl font-bold">Welcome to My App!</h1>
+    <p>This is a simple example of a layout using Radix and Tailwind.</p>
+  </BaseLayout>
 </template>
+
+<script setup>
+import BaseLayout from '@/layouts/default.vue';
+</script>
+
+<style>
+.flex-full-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.flex-full-col {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+body {
+  margin: 0;
+}
+h1, h2, p {
+  margin: 0;
+}
+</style>
