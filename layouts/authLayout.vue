6167a3f1b274a03f764e80ffec038a8e7726fc15
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-07 01:09:50 +0200

    :sparkles: New layout for auth pages like login, sign up and logout.

---
+++ layouts/authLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="relative min-h-screen">
+    <!-- Flex container to center the form -->
+    <div class="relative z-10 flex items-center justify-center min-h-screen">
+      <slot /> <!-- Slot for the LoginForm or SignupForm -->
+    </div>
+
+    <!-- Background image that stays at the bottom left -->
+    <img
+      src="~/assets/img/signupBgLeft.jpg"
+      alt=""
+      class="absolute bottom-0 left-0 h-auto max-w-96"
+    />
+
+    <!-- Background image that stays at the bottom right -->
+    <img
+      src="~/assets/img/signupBgRight.jpg"
+      alt=""
+      class="absolute bottom-0 right-0 h-auto max-w-96"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+@media (max-width: 800px) {
+  img {
+    display: none;
+  }
+}
+@media (min-width: 800px) and (max-width: 1200px) {
+  img {
+    width: 35%;
+  }
+}
+</style>
