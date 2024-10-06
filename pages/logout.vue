80c0ba2b250c7d7105c3c065fce4b63664e0dceb
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-07 01:16:06 +0200

    :heavy_plus_sign: New logout page with the logout component and auth layout.

---
+++ pages/logout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLayout name="auth-layout">
+      <LogoutForm />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
