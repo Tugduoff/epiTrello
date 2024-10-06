9e5874b7f869d90e927b0e3098cf2e847c83fcf7
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-06 22:14:41 +0200

    :heavy_plus_sign: Added a redirection to index.vue whenever we click this button.

--- components/TitleButton.vue
+++ components/TitleButton.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="flex flex-row justify-center items-center px-2 cursor-pointer gap-1.5 hover:bg-slate-200 rounded-sm h-7">
+    <div
+      @click="router.push(menuRoute)"
+      class="flex flex-row justify-center items-center px-2 cursor-pointer gap-1.5 hover:bg-slate-200 rounded-sm h-7">
       <Icon icon="simple-icons:trello" alt="" class="w-[1.1rem] h-[1.1rem] text-slate-600" />
       <h1 class="text-lg font-black font-mono text-slate-600 tracking-tighter text-center justify-center mt-0.5">
         Trello</h1>
@@ -10,6 +12,10 @@
 
 <script lang="ts" setup>
 import { Icon } from '@iconify/vue'
+const router = useRouter();
+const localePath = useLocalePath();
+
+const menuRoute = localePath({ name: 'index' })
 </script>
 
 <style>
