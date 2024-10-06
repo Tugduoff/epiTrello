6c59d2b9af7c12312dda4df896d61a803c4fa9b9
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-07 01:12:41 +0200

    :heavy_plus_sign: Adding auth verification in the user layout.

--- layouts/userLayout.vue
+++ layouts/userLayout.vue
@@ -9,6 +9,23 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
+import Cookies from 'js-cookie'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const localePath = useLocalePath()
+
+onMounted(() => {
+  const token = Cookies.get('token')
+  if (token) {
+    console.log('User is logged in')
+  } else {
+    console.log('User is not logged in')
+    const signupRoute = localePath({ name: 'signup' })
+    router.push(signupRoute)
+  }
+})
 </script>
 
 <style scoped>
